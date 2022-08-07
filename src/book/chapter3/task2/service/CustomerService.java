a1bc5657aee1721e47fac28ebc0dddf373bef32f
chris <slovikovska@gmail.com>
2022-08-07 22:05:39 +0300

    packages renamed and rearranged

--- src/book/chapter3/task2/service/CustomerService.java
+++ src/book/chapter3/task2/service/CustomerService.java
@@ -2,14 +2,12 @@ package book.chapter3.task2.service;
 import book.chapter3.task2.model.Customer;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
-import static java.lang.StringUTF16.charAt;
-
-
 public class CustomerService {
-    private String[] abc = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+    private static String[] abc = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
     public static List<Customer> sortBankAccounts(long startPoint, long finishPoint, List<Customer>allCustomers) {
         List <Customer> result = new ArrayList<>();
         Iterator<Customer> sortBankAccount = allCustomers.iterator();
@@ -37,10 +35,17 @@ public class CustomerService {
         }
     return result;
     }
-
-    public static char charAt(int i, String name) {
-
-    }
-
     */
+   public static List<String> sortAlphabetically(List<Customer>allCustomers) {
+       List<String> result = new ArrayList<>();
+       Iterator<Customer> sortAbc = allCustomers.iterator();
+       while(sortAbc.hasNext()) {
+           Customer customer = sortAbc.next();
+           result.add(customer.getLastName());
+       }
+       Collections.sort(result);
+       return result;
+   }
+
+
 }
